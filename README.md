34b3a5b7da68c30abd9738ac913616634578ec2f
syed Rizvi <rizvi5a@yahoo.com>
2021-03-17 16:37:25 -0500

    arrays of small, capital, numbers and special charachters are added

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Setting up file structure for Password Generation Assignment 
+Password Generation Plan
+First create an array of mixed characters, numbers, and special characters from which they will generate a password by making a string of at least 8 characters long. The password string characters will be selected randomly .The password can only be 128 characters long.
+The arrays of small, capital, numbers and speccial charachetes are added.  
--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 // Assignment Code
+
+var smallLetters = [
+  'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','u','r','s','t','x','y','z'];
+
+var CapLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','U','R','S','T','X','Y','Z'];
+var  numbersChar = ['1','2','3','4','5','6','7','8','9','0'];
+
+var specialChar = ['!','@','#','$','%','^','&','*','(',')','+']
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
