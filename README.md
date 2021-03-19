605a6e091fdce8fa718ff4b112738384fa8ad01a
syed Rizvi <rizvi5a@yahoo.com>
2021-03-19 10:54:56 -0500

    password options code is tested

--- README.md
+++ README.md
@@ -7,21 +7,12 @@
 
 #  Password Generation Steps:
 * The arrays of small, capital, numbers and speccial charachetes are generated. 
-* 
+* The code for password length generation options is added and checked.
+* The code for password options( small & capital letters, numbers ans specials characters) is added and checked.
+* The password length and user options are stored in "passwordOption object".
+* Random number selction function is setup for arrays.
 
-## Your Task
 
-This week’s homework requires you to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
-
-The password can include special characters. If you’re unfamiliar with these, see this [list of password special characters](https://www.owasp.org/index.php/Password_special_characters) from the OWASP Foundation.
-
-## User Story
-
-```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
 
 ## Acceptance Criteria
 
--- js/script.js
+++ js/script.js
@@ -6,15 +6,15 @@ var smallLetters = [
 var capLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','U','R','S','T','X','Y','Z'];
 var  numbersChar = ['1','2','3','4','5','6','7','8','9','0'];
 
-var specialChar = ['!','@','#','$','%','^','&','*','(',')','+']
+var specialChar = ['!','@','#','$','%','^','&','*','(','+'];
 
 //  Generate password button is clicked 
 // make getPasswordOption  function and add evenentListener to clcik  
-var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", function() {
-  getPasswordOptions()
-}
-);
+// var generateBtn = document.querySelector("#generate");
+ // generateBtn.addEventListener("click", function() {
+ // getPasswordOptions()
+//}
+// );
 // password requiremnets function is called when generateBTN is pressed
 // propomts are introduced in the function
 function getPasswordOptions() {
@@ -42,23 +42,91 @@ if (length > 128) {
   return;
 }
 
+// Confirm() function is used to store and include in the password selection array
+// Variable to store boolean regarding the inclusion of special characters
+var smallLetters = confirm(
+  'Click OK to confirm including small Letters.'
+);
+
+// Variable to store boolean regarding the inclusion of special characters
+var capLetters = confirm(
+  'Click OK to confirm including capital letters.'
+);
+// Variable to store boolean regarding the inclusion of special characters
+var numbersChar = confirm(
+  'Click OK to confirm to include numbers'
+);
+
+
+// Variable to store boolean regarding the inclusion of special characters
+var  specialChar = confirm(
+  'Click OK to confirm including special characters.'
+);
+
+// The following code checks whether users did not include any of the options:
+// returns back to password options
+ if ( 
+  smallLetters === false &&
+   capLetters === false &&
+   numbersChar === false &&
+   specialChar === false
+  
+ ) {
+  alert('user must select one of the option');
+   return;
 }
 
 
-  
+// Object to store user inpput
+var passwordOptions = {
+  length: length,
+  smallLetters: smallLetters,
+  capLetters: capLetters,
+  numbersChar: numbersChar,
+  specialChar: specialChar
+};
+
+return  passwordOptions;
+}
 
 
+// Function for getting a random element from an array
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
 
+  return randElement;
+}
 
+// Function to generate password with user input
+function generatePassword() {
+  var options = getPasswordOptions();
+ 
+}
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
+//function writePassword() {
+//  var password = generatePassword();
+//  var passwordText = document.querySelector('#password');
+
+//  passwordText.value = password;
+//}
+
+// Add event listener to generate button
+// generateBtn.addEventListener('click', writePassword);
+
+
+
+/* Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
-
+}``
+*/
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+ generateBtn.addEventListener("click", generatePassword);
